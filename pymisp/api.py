d1db9a74eeaaf8e8f40ae924ebd5b7eade69d6b2
Raphaël Vinot <raphael@vinot.info>
2015-11-05 09:35:43 +0100

    Add add_ipsrc

--- pymisp/api.py
+++ pymisp/api.py
@@ -392,6 +392,11 @@ class PyMISP(object):
         attributes.append(self._prepare_full_attribute(category, 'ip-dst', ipdst, to_ids, comment, distribution))
         return self._send_attributes(event, attributes, proposal)
 
+    def add_ipsrc(self, event, ipsrc, category='Network activity', to_ids=True, comment=None, distribution=None, proposal=False):
+        attributes = []
+        attributes.append(self._prepare_full_attribute(category, 'ip-src', ipsrc, to_ids, comment, distribution))
+        return self._send_attributes(event, attributes, proposal)
+
     def add_hostname(self, event, hostname, category='Network activity', to_ids=True, comment=None, distribution=None, proposal=False):
         attributes = []
         attributes.append(self._prepare_full_attribute(category, 'hostname', hostname, to_ids, comment, distribution))
