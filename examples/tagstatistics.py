e70cc7a985b84605145cbe7a760304c86bcdef1b
Alexandre Dulaunoy <a@foo.be>
2016-09-12 13:45:37 +0200

    Toggle flag instead of value

--- examples/tagstatistics.py
+++ examples/tagstatistics.py
@@ -18,8 +18,8 @@ def init(url, key):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Get statistics from tags.')
-    parser.add_argument("-p", "--percentage", help="An optional field, if set, it will return the results in percentages, otherwise it returns exact count.")
-    parser.add_argument("-n", "--namesort", help="An optional field, if set, values are sort by the namespace, otherwise the sorting will happen on the value.")
+    parser.add_argument("-p", "--percentage", action='store_true', default=None, help="An optional field, if set, it will return the results in percentages, otherwise it returns exact count.")
+    parser.add_argument("-n", "--namesort", action='store_true', default=None, help="An optional field, if set, values are sort by the namespace, otherwise the sorting will happen on the value.")
     args = parser.parse_args()
 
     misp = init(misp_url, misp_key)
