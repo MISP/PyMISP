abd836babb81a325562afbffcb3f9de23fa7075d
Raphaël Vinot <raphael@vinot.info>
2016-10-25 17:28:55 -0400

    Add simple script to push MISP events into Neo4j

--- pymisp/__init__.py
+++ pymisp/__init__.py
@@ -3,3 +3,4 @@ __version__ = '2.4.53'
 from .exceptions import PyMISPError, NewEventError, NewAttributeError, MissingDependency, NoURL, NoKey
 from .api import PyMISP
 from .mispevent import MISPEvent, MISPAttribute, EncodeUpdate, EncodeFull
+from .tools.neo4j import Neo4j
