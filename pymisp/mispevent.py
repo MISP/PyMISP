d48f2481763a4bf8eb7c075fdeedf0e438708c6b
Raphaël Vinot <raphael@vinot.info>
2016-10-27 15:58:58 -0400

    Fix Python2 - Python3 support

--- pymisp/mispevent.py
+++ pymisp/mispevent.py
@@ -20,10 +20,11 @@ from .exceptions import PyMISPError, NewEventError, NewAttributeError
 
 # Least dirty way to support python 2 and 3
 try:
-    warnings.warn("You're using python 2, it is strongly recommended to use python >=3.3")
     basestring
+    warnings.warn("You're using python 2, it is strongly recommended to use python >=3.3")
 except NameError:
     basestring = str
+    unicode = str
 
 
 class MISPAttribute(object):
