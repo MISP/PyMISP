857cd40ea20d9c79282170ad6185dc53f5c9a9c6
Raphaël Vinot <raphael@vinot.info>
2016-10-27 16:29:56 -0400

    Update import

--- pymisp/__init__.py
+++ pymisp/__init__.py
@@ -4,3 +4,4 @@ from .exceptions import PyMISPError, NewEventError, NewAttributeError, MissingDe
 from .api import PyMISP
 from .mispevent import MISPEvent, MISPAttribute, EncodeUpdate, EncodeFull
 from .tools.neo4j import Neo4j
+from .tools import stix
