5c74a2474fa1600776bd64a3d42c9f0c9f11d7c6
CheYenBzh <CheYenBzh@users.noreply.github.com>
2017-05-29 14:03:21 +0200

    Create fetch_events_feed.py

---
+++ examples/fetch_events_feed.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from keys import misp_url, misp_key
+import argparse
+from pymisp import PyMISP
+
+# For python2 & 3 compat, a bit dirty, but it seems to be the least bad one
+try:
+    input = raw_input
+except NameError:
+    pass
+    
+def init(url, key):
+    return PyMISP(url, key, False, 'json', debug=False)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Fetch all events from a feed.')
+    parser.add_argument("-f", "--feed", required=True, help="feed's ID to be fetched.")
+    args = parser.parse_args()
+    
+    misp = init(misp_url, misp_key)
+    misp.fetch_feed(args.feed)
