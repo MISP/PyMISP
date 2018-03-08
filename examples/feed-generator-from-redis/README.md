188c452a390a63e13ebb1629de972837ae3da269
Sami Mokaddem <mokaddem.sami@gmail.com>
2018-03-08 12:01:35 +0100

    Init draft of redis to feed

---
+++ examples/feed-generator-from-redis/README.md
@@ -0,0 +1,13 @@
+# What
+
+This python script can be used to generate a MISP feed based on data stored in redis.
+
+# Installation
+
+````
+git clone https://github.com/CIRCL/PyMISP
+cd examples/feed-generator-from-redis
+cp settings-default.py settings.py
+vi settings.py #adjust your settings
+python3 generate.py
+````
---
+++ examples/feed-generator-from-redis/settings.default.py
@@ -0,0 +1,38 @@
+# Your redis server
+host='127.0.0.1'
+port=6379
+db=0
+## The keynames to POP element from
+keyname_pop='misp_feed_generator_key'
+
+# The output dir for the feed. This will drop a lot of files, so make
+# sure that you use a directory dedicated to the feed
+outputdir = 'output'
+
+# Event meta data
+## Required
+### The organisation id that generated this feed
+org_name='myOrg'
+### Your organisation UUID
+org_uuid=''
+### The daily event name to be used in MISP. (e.g. honeypot_1, will produce each day an event of the form honeypot_1 dd-mm-yyyy)
+daily_event_name='PyMISP default event name'
+
+## Optional
+analysis=0
+threat_level_id=3
+published=False
+Tag=[{
+        "colour": "#ffffff",
+        "name": "tlp:white"
+    }]
+
+# Others
+## Redis pooling time
+sleep=1
+## The redis list keyname in which to put items that generated an error
+keyname_error='feed-generation-error'
+## Display an animation while adding element to MISP
+allow_animation=True
+## How frequent the event should be written on disk
+flushing_interval=60*5
