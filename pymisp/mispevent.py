644337e615021141b7a081ee23be81708fe8ff58
Raphaël Vinot <raphael@vinot.info>
2018-07-05 18:06:58 +0200

    fix: Improve error message in case the object template is unknown.

--- pymisp/mispevent.py
+++ pymisp/mispevent.py
@@ -846,6 +846,8 @@ class MISPObjectAttribute(MISPAttribute):
         if self.to_ids is None:
             # Same for the to_ids flag
             self.to_ids = self._definition.get('to_ids')
+        if not self.type:
+            raise NewAttributeError("The type of the attribute is required. Is the object template missing?")
         super(MISPObjectAttribute, self).from_dict(**dict(self, **kwargs))
 
     def __repr__(self):
