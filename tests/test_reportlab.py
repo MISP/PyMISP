5d5ea36e812ebb0cf341885d572b7fd183581f57
Falconieri <vincent.falconieri@gmail.com>
2019-03-04 14:44:33 +0100

    fix: [exportpdf] tests update

--- tests/test_reportlab.py
+++ tests/test_reportlab.py
@@ -9,7 +9,7 @@ import unittest
 from pymisp import MISPEvent
 from pymisp.tools import reportlab_generator
 
-manual_testing = True
+manual_testing = False
 
 if sys.version_info < (3, 6):
     print('This test suite requires Python 3.6+, breaking.')
@@ -33,7 +33,7 @@ class TestMISPEvent(unittest.TestCase):
         self.storage_folder = self.root + "reportlab_testoutputs/"
         self.storage_image_folder = self.root + "reportlab_test_image_outputs/"
         self.moduleconfig = ["MISP_base_url_for_dynamic_link", "MISP_name_for_metadata", "Activate_textual_description",
-                             "Activate_galaxy_description", "Activate_related_events"]
+                             "Activate_galaxy_description", "Activate_related_events", "Activate_internationalization_fonts"]
 
 
     def init_event(self):
@@ -244,22 +244,6 @@ class TestMISPEvent(unittest.TestCase):
                 reportlab_generator.convert_event_in_pdf_buffer(self.mispevent, config),
                 self.storage_folder + "galaxy_1.pdf")
 
-    def test_galaxy_1(self):
-        if self.check_python_2():
-            self.assertTrue(True)
-        else:
-            config = {}
-            config[self.moduleconfig[0]] = "http://localhost:8080"
-            config[self.moduleconfig[1]] = "My Wonderful CERT"
-            config[self.moduleconfig[2]] = True
-            config[self.moduleconfig[3]] = True
-
-            self.init_event()
-            self.mispevent.load_file(self.test_folder + 'galaxy_1.json')
-            reportlab_generator.register_value_to_file(
-                reportlab_generator.convert_event_in_pdf_buffer(self.mispevent, config),
-                self.storage_folder + "extended_description.pdf")
-
     def test_related_events(self):
         if self.check_python_2():
             self.assertTrue(True)
@@ -304,6 +288,7 @@ class TestMISPEvent(unittest.TestCase):
             config[self.moduleconfig[2]] = True
             config[self.moduleconfig[3]] = True
             config[self.moduleconfig[4]] = True
+            config[self.moduleconfig[5]] = True
 
             self.init_event()
             self.mispevent.load_file(self.test_folder + 'japanese_test.json')
@@ -383,6 +368,7 @@ class TestMISPEvent(unittest.TestCase):
             config[self.moduleconfig[2]] = True
             config[self.moduleconfig[3]] = True
             config[self.moduleconfig[4]] = True
+            config[self.moduleconfig[5]] = True
 
 
             file_nb = str(len(os.listdir(self.test_batch_folder)))
