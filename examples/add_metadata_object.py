167f30c243a41adaefbcad84a0b866ec2f023476
vvx7 <vvx7@mailserver.lab3.internal>
2019-04-15 10:39:15 +0200

    chg: [tools] Added license to tool.

--- examples/add_metadata_object.py
+++ examples/add_metadata_object.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#
+#
+#    A simple tool to add file metadata objects to a MISP event.
+#    Copyright (C) 2019 Roger Johnston
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from pymisp import PyMISP
 from pymisp.tools.mimetypeobject import MIMETypeObject
