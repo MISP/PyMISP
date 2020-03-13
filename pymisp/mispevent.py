8cf3887d54cd883fdba0b82d7be38eb9f7fb41d7
Raphaël Vinot <raphael@vinot.info>
2020-03-13 11:02:48 +0100

    fix: Incorrect expectation of attribute value to be a str
    Fix #553

--- pymisp/mispevent.py
+++ pymisp/mispevent.py
@@ -1290,7 +1290,7 @@ class MISPEvent(AbstractMISP):
             if ((hasattr(a, 'id') and a.id == attribute_identifier)
                     or (hasattr(a, 'uuid') and a.uuid == attribute_identifier)
                     or (hasattr(a, 'value') and attribute_identifier == a.value
-                        or attribute_identifier in a.value.split('|'))):
+                        or (isinstance(a.value, str) and attribute_identifier in a.value.split('|')))):
                 a.add_tag(tag)
                 attributes.append(a)
 
