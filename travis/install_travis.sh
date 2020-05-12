e17a90c9545dade365d082e1ef1c3be035ca83bd
Raphaël Vinot <raphael@vinot.info>
2020-05-12 22:35:58 +0200

    chg: Bump travis install

--- travis/install_travis.sh
+++ travis/install_travis.sh
@@ -5,4 +5,4 @@ set -x
 
 # We're in python3, installing with poetry.
 pip3 install poetry
-poetry install -E fileobjects -E openioc -E virustotal -E docs -E pdfexport
+poetry install -E fileobjects -E openioc -E virustotal -E docs -E pdfexport -E url
