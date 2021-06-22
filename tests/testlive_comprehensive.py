b963c417168be334b2ae1353551140cce2f592eb
Jakub Onderka <jakub.onderka@gmail.com>
2021-06-22 17:48:53 +0200

    new: Method `update_sharing_group`

--- tests/testlive_comprehensive.py
+++ tests/testlive_comprehensive.py
@@ -2096,6 +2096,9 @@ class TestComprehensive(unittest.TestCase):
         self.assertTrue(self.admin_misp_connector.sharing_group_exists(sharing_group))
         self.assertTrue(self.admin_misp_connector.sharing_group_exists(sharing_group.id))
         self.assertTrue(self.admin_misp_connector.sharing_group_exists(sharing_group.uuid))
+        r = self.admin_misp_connector.update_sharing_group({"releasability": "Testing updated"}, sharing_group)
+        self.assertEqual(sharing_group.releasability, 'Testing updated')
+
         # add org
         r = self.admin_misp_connector.add_org_to_sharing_group(sharing_group,
                                                                self.test_org, extend=True)
