f11681af528f147ca6939706a085e833ab7dded3
Raphaël Vinot <raphael@vinot.info>
2024-01-17 13:38:00 +0100

    fix: Python < 3.10 support on typing

--- pymisp/api.py
+++ pymisp/api.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TypeVar, Any, Mapping, Iterable, MutableMapping
+from typing import TypeVar, Any, Mapping, Iterable, MutableMapping, Union
 from datetime import date, datetime
 import csv
 from pathlib import Path
@@ -57,7 +57,7 @@ except ImportError:
 
 SearchType = TypeVar('SearchType', str, int)
 # str: string to search / list: values to search (OR) / dict: {'OR': [list], 'NOT': [list], 'AND': [list]}
-SearchParameterTypes = TypeVar('SearchParameterTypes', str, list[str | int], dict[str, str | int])
+SearchParameterTypes = TypeVar('SearchParameterTypes', str, list[Union[str, int]], dict[str, Union[str, int]])
 
 ToIDSType = TypeVar('ToIDSType', str, int, bool)
 
